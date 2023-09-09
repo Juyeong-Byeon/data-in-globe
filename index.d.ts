5776c9d64b6f387d3f4d1eaed104d1eb237ac5d1
Juyeong Byeon (Matthew) <35381940+Juyeong-Byeon@users.noreply.github.com>
2023-09-09 13:32:08 +0900

    feat: implement updateData interface (#10)

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,17 @@
 declare class GlobalData<Data> {
-  private listeners;
-  private data;
-  constructor(data: Data);
-  getData(): Readonly<Data>;
-  setData(state: Data): void;
-  subscribe(listener: Function): () => boolean;
+    private listeners;
+    private data;
+    constructor(data: Data);
+    getData(): Readonly<Data>;
+    setData(state: Data): void;
+    updateData(updater: (state: Data) => Data): void;
+    subscribe(listener: Function): () => boolean;
+    private notify;
 }
-declare function useGlobalData<State>(
-  globalData: GlobalData<State>
-): [Readonly<State>, (state: State) => void];
+declare function useGlobalData<State>(globalData: GlobalData<State>): [
+    Readonly<State>,
+    (state: State) => void,
+    (updater: (state: State) => State) => void
+];
 export { GlobalData, useGlobalData };
 //# sourceMappingURL=index.d.ts.map
