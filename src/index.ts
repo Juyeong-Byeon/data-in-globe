fdb51cff8fed1ce3f0d4d43696bfaf853ab0933a
Juyeong-Byeon <wndudqus@naver.com>
2023-09-09 13:28:50 +0900

    feat: implement updateData interface

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import { useCallback } from "react";
 import { useSyncExternalStore } from "use-sync-external-store/shim";
-// import cloneDeep from "lodash/cloneDeep";
+import cloneDeep from "lodash/cloneDeep";
+import { produce } from "immer";
 
 class GlobalData<Data> {
-  private listeners: Set<Function> = new Set();
+  private listeners: Set<Function>;
   private data: Readonly<Data>;
+
   constructor(data: Data) {
-    this.data = Object.freeze(data);
+    this.listeners = new Set();
+    this.data = Object.freeze(cloneDeep(data));
   }
 
   getData() {
@@ -14,25 +17,45 @@ class GlobalData<Data> {
   }
 
   setData(state: Data) {
-    this.data = Object.freeze(state);
-    this.listeners.forEach((listener) => listener());
+    this.data = Object.freeze(cloneDeep(state));
+    this.notify();
+  }
+
+  updateData(updater: (state: Data) => Data) {
+    this.data = Object.freeze(produce(this.data, updater));
+    this.notify();
   }
 
   subscribe(listener: Function) {
     this.listeners.add(listener);
     return () => this.listeners.delete(listener);
   }
+
+  private notify() {
+    this.listeners.forEach((listener) => listener());
+  }
 }
 
 function useGlobalData<State>(
   globalData: GlobalData<State>
-): [Readonly<State>, (state: State) => void] {
-  const data = useSyncExternalStore(globalData.subscribe, globalData.getData);
+): [
+  Readonly<State>,
+  (state: State) => void,
+  (updater: (state: State) => State) => void
+] {
+  const data = useSyncExternalStore<State>(
+    useCallback(globalData.subscribe.bind(globalData), [globalData]),
+    useCallback(globalData.getData.bind(globalData), [globalData])
+  );
   const setData = useCallback(globalData.setData.bind(globalData), [
     globalData,
   ]);
 
-  return [data, setData];
+  const updateData = useCallback(globalData.updateData.bind(globalData), [
+    globalData,
+  ]);
+
+  return [data, setData, updateData];
 }
 
 export { GlobalData, useGlobalData };
